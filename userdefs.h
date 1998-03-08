de4255c7cb09b683cbb6652962f56c31c35ffa52
Thomas E. Dickey <dickey@invisible-island.net>
1998-03-07 22:00:00 -0500

    snapshot of project "lynx", label v2-8rel_1

--- userdefs.h
+++ userdefs.h
@@ -1212,7 +1212,7 @@
  * the version definition with the Project Version on checkout. Just
  * ignore it. - kw */
 /* $Format: "#define LYNX_VERSION \"$ProjectVersion$\""$ */
-#define LYNX_VERSION "2.8pre.5"
+#define LYNX_VERSION "2.8rel.1"
 
 #ifndef MAXINT
 #define MAXINT 2147483647	/* max integer */
