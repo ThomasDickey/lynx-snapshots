b49eceeadd5f4c4c1327500ac6c7d84859e94b47
Thomas E. Dickey <dickey@invisible-island.net>
2000-02-15 12:03:16 -0500

    snapshot of project "lynx", label v2-8-3dev_20

--- userdefs.h
+++ userdefs.h
@@ -1302,12 +1302,12 @@
  * the version definition with the Project Version on checkout.  Just
  * ignore it. - kw */
 /* $Format: "#define LYNX_VERSION \"$ProjectVersion$\""$ */
-#define LYNX_VERSION "2.8.3dev.19"
+#define LYNX_VERSION "2.8.3dev.20"
 #define LYNX_WWW_HOME "http://lynx.browser.org/"
 #define LYNX_WWW_DIST "http://www.slcc.edu/lynx/current/"
 #define LYNX_RELEASE FALSE
 /* $Format: "#define LYNX_DATE \"$ProjectDate$\""$ */
-#define LYNX_DATE "Mon, 14 Feb 2000 20:07:39 -0800"
+#define LYNX_DATE "Tue, 15 Feb 2000 04:01:32 -0800"
 #define LYNX_DATE_OFF 5		/* truncate the automatically-generated date */
 #define LYNX_DATE_LEN 11	/* truncate the automatically-generated date */
 #define LYNX_RELEASE_DATE "1999"
