f26faa9e423c8611092fb08f9e3f53b16e6baa52
Thomas E. Dickey <dickey@invisible-island.net>
2006-10-11 00:15:00 -0400

    snapshot of project "lynx", label v2-8-6rel_1

--- userdefs.h
+++ userdefs.h
@@ -1360,11 +1360,11 @@
  * the version definition with the Project Version on checkout.  Just
  * ignore it. - kw */
 /* $Format: "#define LYNX_VERSION \"$ProjectVersion$\""$ */
-#define LYNX_VERSION "2.8.6pre.6"
+#define LYNX_VERSION "2.8.6rel.1"
 #define LYNX_WWW_HOME "http://lynx.isc.org/"
 #define LYNX_WWW_DIST "http://lynx.isc.org/current/"
 /* $Format: "#define LYNX_DATE \"$ProjectDate$\""$ */
-#define LYNX_DATE "Tue, 10 Oct 2006 16:39:50 -0700"
+#define LYNX_DATE "Tue, 10 Oct 2006 17:15:23 -0700"
 #define LYNX_DATE_OFF 5		/* truncate the automatically-generated date */
 #define LYNX_DATE_LEN 11	/* truncate the automatically-generated date */
 
