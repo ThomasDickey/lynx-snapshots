e7b26e02384cb53c4cb9ce12996787796444de21
Thomas E. Dickey <dickey@invisible-island.net>
2007-08-03 00:21:48 -0400

    snapshot of project "lynx", label v2-8-7dev_7

--- userdefs.h
+++ userdefs.h
@@ -1,5 +1,5 @@
 /*
- * $LynxId: userdefs.h,v 1.228 2007/08/03 00:15:38 tom Exp $
+ * $LynxId: userdefs.h,v 1.229 2007/08/03 00:21:48 tom Exp $
  *
  * Lynx - Hypertext navigation system
  *
@@ -1371,11 +1371,11 @@
  * the version definition with the Project Version on checkout.  Just
  * ignore it. - kw */
 /* $Format: "#define LYNX_VERSION \"$ProjectVersion$\""$ */
-#define LYNX_VERSION "2.8.7dev.6"
+#define LYNX_VERSION "2.8.7dev.7"
 #define LYNX_WWW_HOME "http://lynx.isc.org/"
 #define LYNX_WWW_DIST "http://lynx.isc.org/current/"
 /* $Format: "#define LYNX_DATE \"$ProjectDate$\""$ */
-#define LYNX_DATE "Thu, 02 Aug 2007 16:24:27 -0700"
+#define LYNX_DATE "Thu, 02 Aug 2007 17:04:46 -0700"
 #define LYNX_DATE_OFF 5		/* truncate the automatically-generated date */
 #define LYNX_DATE_LEN 11	/* truncate the automatically-generated date */
 
