3a21c5bc3417b199af968e4ff52e80ca67ee66d7
Thomas E. Dickey <dickey@invisible-island.net>
2013-10-01 20:56:17 -0400

    snapshot of project "lynx", label v2-8-8dev_16b

--- WWW/Library/Implementation/HTTP.c
+++ WWW/Library/Implementation/HTTP.c
@@ -1,5 +1,5 @@
 /*
- * $LynxId: HTTP.c,v 1.128 2013/05/05 19:36:45 tom Exp $
+ * $LynxId: HTTP.c,v 1.129 2013/10/01 15:09:29 tom Exp $
  *
  * HyperText Tranfer Protocol	- Client implementation		HTTP.c
  * ==========================
@@ -717,7 +717,10 @@ static int HTLoadHTTP(const char *arg,
 	    handle->options |= SSL_OP_NO_TLSv1;
 #if OPENSSL_VERSION_NUMBER >= 0x0090806fL && !defined(OPENSSL_NO_TLSEXT)
 	} else {
-	    SSL_set_tlsext_host_name(handle, ssl_host);
+	    int ret = SSL_set_tlsext_host_name(handle, ssl_host);
+
+	    CTRACE((tfp, "...called SSL_set_tlsext_host_name(%s) ->%d\n",
+		    ssl_host, ret));
 #endif
 	}
 #endif
