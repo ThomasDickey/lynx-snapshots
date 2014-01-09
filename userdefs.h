7c7d8c9582cea3ed0ff52f6e7d212fa85b3cd75f
Thomas E. Dickey <dickey@invisible-island.net>
2014-01-09 00:29:11 -0500

    snapshot of project "lynx", label v2-8-8pre_2a

--- userdefs.h
+++ userdefs.h
@@ -1,12 +1,12 @@
 /*
- * $LynxId: userdefs.h,v 1.283 2013/12/18 01:56:13 tom Exp $
+ * $LynxId: userdefs.h,v 1.285 2014/01/09 00:25:45 tom Exp $
  *
  * Lynx - Hypertext navigation system
  *
  *   (c) Copyright 1992, 1993, 1994 University of Kansas
  *	 1995, 1996: GNU General Public License
  *
- *   Copyright 1996-2012,2013 Thomas E. Dickey and Lynx Developers Group
+ *   Copyright 1996-2013,2014 Thomas E. Dickey and Lynx Developers Group
  *   Note: GNU General Public License is not a copyright.
  */
 
@@ -1442,7 +1442,7 @@
  * the version definition with the Project Version on checkout.  Just
  * ignore it. - kw */
 /* $Format: "#define LYNX_VERSION \"$ProjectVersion$\""$ */
-#define LYNX_VERSION "2.8.8pre.2"
+#define LYNX_VERSION "2.8.8pre.3"
 #define LYNX_WWW_HOME "http://lynx.isc.org/"
 #define LYNX_WWW_DIST "http://lynx.isc.org/current/"
 /* $Format: "#define LYNX_DATE \"$ProjectDate$\""$ */
