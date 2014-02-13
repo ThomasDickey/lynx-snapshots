5f1307fdb4466cc4a76a6ca9f46e0399ec0459bc
Thomas E. Dickey <dickey@invisible-island.net>
2014-02-13 13:33:21 -0500

    snapshot of project "lynx", label v2-8-8pre_4d

--- scripts/conf.mingw.sh
+++ scripts/conf.mingw.sh
@@ -1,9 +1,15 @@
 #!/bin/bash.exe
+# $LynxId: conf.mingw.sh,v 1.2 2014/02/13 13:30:54 tom Exp $
+
+LYNX_DEV=d
+LYNX_TOP=/cygwin/mingw/lynx2.8.7dev.13
+LYNX_ETC=$LYNX_TOP/lynx-conf
+
 LDFLAGS="-mno-cygwin -static -L/mingw/lib" \
 LIBS="-lwsock32 -lgdi32 -lmsvcrt -liconv" \
 CFLAGS="-mno-cygwin -I/mingw/include -I/mingw/include/openssl -W -Wall -O3 -D_WINDOWS -DSH_EX -DWIN_EX -DNOUSERS -DNOSIGHUP -DDOSPATH -DUSE_ALT_BLAT_MAILER -DBOXHORI=0 -DBOXVERT=0" \
 CPPFLAGS="-mno-cygwin -I/mingw/include -I/mingw/include/openssl" \
-./configure --prefix=/d/cygwin/mingw/lynx2.8.7dev.13/lynx-conf \
+./configure --prefix=/$LYNX_DEV$LYNX_ETC \
 --host=mingw32 \
 --disable-dired-override \
 --disable-full-paths \
@@ -19,11 +25,11 @@ CPPFLAGS="-mno-cygwin -I/mingw/include -I/mingw/include/openssl" \
 --enable-nls \
 --enable-vertrace \
 --includedir=/mingw/include \
---sysconfdir=/d/cygwin/mingw/lynx2.8.7dev.13/lynx-conf \
---datadir=/d/cygwin/mingw/lynx2.8.7dev.13/lynx-conf \
+--sysconfdir=/$LYNX_DEV$LYNX_ETC \
+--datadir=/$LYNX_DEV$LYNX_ETC \
 --with-bzlib \
---with-cfg-file=d:/cygwin/mingw/lynx2.8.7dev.13/lynx-conf/lynx.cfg \
---with-lss-file=d:/cygwin/mingw/lynx2.8.7dev.13/lynx-conf/lynx.lss \
+--with-cfg-file=$LYNX_DEV:$LYNX_ETC/lynx.cfg \
+--with-lss-file=$LYNX_DEV:$LYNX_ETC/lynx.lss \
 --with-mime-libdir=c:/ \
 --with-nls-datadir=d:/cygwin/mingw/share \
 --with-pkg-config=no \
