751c74175885c86180beabd029aba7728d78b6d2
Thomas E. Dickey <dickey@invisible-island.net>
2015-04-13 00:52:36 -0400

    snapshot of project "lynx", label v2-8-9dev_5

--- userdefs.h
+++ userdefs.h
@@ -1,5 +1,5 @@
 /*
- * $LynxId: userdefs.h,v 1.304 2015/03/22 15:16:20 tom Exp $
+ * $LynxId: userdefs.h,v 1.305 2015/04/13 00:52:36 tom Exp $
  *
  * Lynx - Hypertext navigation system
  *
@@ -1446,7 +1446,7 @@
 #define LYNX_WWW_HOME "http://lynx.isc.org/"
 #define LYNX_WWW_DIST "http://lynx.isc.org/current/"
 /* $Format: "#define LYNX_DATE \"$ProjectDate$\""$ */
-#define LYNX_DATE "Sun, 25 Jan 2015 11:42:38 -0800"
+#define LYNX_DATE "Sun, 12 Apr 2015 17:52:36 -0700"
 #define LYNX_DATE_OFF 5		/* truncate the automatically-generated date */
 #define LYNX_DATE_LEN 11	/* truncate the automatically-generated date */
 
